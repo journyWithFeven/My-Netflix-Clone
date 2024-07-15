c42a17eb4e33c88da30716fff73217f35e0d8ecf
journyWithFeven <fevenyeyo@gmail.com>
2024-07-15 21:45:53 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import "../src/index.css";
+import Home from "./Pages/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Rows/Row/Row.jsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import "./Row.css";
+import axios from "../../Utils/axios";
+import movieTrailer from "movie-trailer";
+import YouTube from "react-youtube";
+
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+  const base_url = "https://image.tmdb.org/t/p/original";
+
+  useEffect(() => {
+    (async () => {
+      try {
+        console.log(fetchUrl);
+        const request = await axios.get(fetchUrl);
+        console.log(request);
+        setMovies(request.data.results);
+      } catch (error) {
+        console.log("error", error);
+      }
+    })();
+  }, [fetchUrl]);
+
+  useEffect(() => {
+    const loadYouTubeIframeAPI = () => {
+      const tag = document.createElement("script");
+      tag.src = "https://www.youtube.com/iframe_api";
+      const firstScriptTag = document.getElementsByTagName("script")[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    };
+
+    if (!window.YT) {
+      loadYouTubeIframeAPI();
+    }
+  }, []);
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || movie?.title || movie?.original_name)
+        .then((url) => {
+          console.log(url);
+          const urlParams = new URLSearchParams(new URL(url).search);
+          console.log(urlParams);
+          console.log(urlParams.get("v"));
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  return (
+    <div className="row">
+      <h1>{title}</h1>
+      <div className="row_posters">
+        {movies &&
+          movies.length > 0 &&
+          movies.map((movie, index) => (
+            <img
+              onClick={() => handleClick(movie)}
+              key={index}
+              src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+              className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            />
+          ))}
+      </div>
+      <div style={{ padding: "40px" }}>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
